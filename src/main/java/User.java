5ee5a7f77a5879e383ddf37d3d47c22fe5518bb3
Carlos Cunha <cunha.cas@gmail.com>
2020-02-14 08:18:06 +0000

    Init

---
+++ src/main/java/User.java
@@ -0,0 +1,25 @@
+public class User {
+    String name;
+    String address;
+
+    public User(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
